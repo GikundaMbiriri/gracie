de334bdde85d0da2351d5637665250391a4daa70
Your Name <you@example.com>
2020-08-25 19:20:49 +0300

    first commit

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="https://en.wikipedia.org/wiki/Kenya">
           Navbar
         </a>
         <button
