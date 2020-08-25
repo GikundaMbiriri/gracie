f993ff2ca27558d79c2513b34e011935171d8d46
Your Name <you@example.com>
2020-08-25 19:17:05 +0300

    first commit

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -22,17 +22,26 @@ const navbar = () => {
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <a
+                className="nav-link"
+                href="https://en.wikipedia.org/wiki/Kenya"
+              >
                 Home <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a
+                className="nav-link"
+                href="https://en.wikipedia.org/wiki/Kenya"
+              >
                 Link
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">
+              <a
+                className="nav-link disabled"
+                href="https://en.wikipedia.org/wiki/Kenya"
+              >
                 Disabled
               </a>
             </li>
