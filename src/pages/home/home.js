c08ce1d9ccf45453165d82663150fc89da837420
Your Name <you@example.com>
2020-08-28 10:31:22 +0300

    first commit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,7 @@ import pic1 from "../../images/pic1.jpg";
 import pic2 from "../../images/pic2.jpg";
 import pic5 from "../../images/pic5.jpg";
 import "./home.css";
-
+import { WhatsappShareButton } from "react-share";
 const home = () => {
   const ww = window.screen.width;
   return (
@@ -119,7 +119,55 @@ const home = () => {
               <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">
                 Read more
               </a>
-              <i className="fas fa-share-alt"></i>
+
+              <div data-toggle="modal" data-target="#exampleModal">
+                <i className="fas fa-share-alt"></i>
+              </div>
+
+              <div
+                class="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">
+                        Modal title
+                      </h5>
+                      <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      {" "}
+                      <WhatsappShareButton url="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">
+                        yt
+                      </WhatsappShareButton>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button type="button" class="btn btn-primary">
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div
