e844e9b452c0834986f535d0a8d956016fbc082b
Your Name <you@example.com>
2020-09-29 00:15:03 +0300

    'ff'

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Navbar />
 
           <Switch>
-            <Route exact path="/gh-pages-url" component={home} />
+            <Route exact path="/" component={home} />
             <Route exact path="/tiny" component={Tiny} />
             <Route exact path="/solo/:id" component={Solo} />
 
