adb2cad608a70f6993756b0323a2d5dabc02e009
Your Name <you@example.com>
2020-09-28 18:23:05 +0300

    y

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,11 +3,7 @@ import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import Btn from "./Modal/Index";
-import Solo from "../pages/solo";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+
 import dayjs from "dayjs";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
