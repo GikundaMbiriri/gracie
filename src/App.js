fd5a3955529a1caba9f6d59df214c98984b309f4
Your Name <you@example.com>
2020-09-29 11:53:27 +0300

    'some'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import home from "./pages/home/home";
 import Tiny from "./pages/tiny";
 import Solo from "./pages/solo";
