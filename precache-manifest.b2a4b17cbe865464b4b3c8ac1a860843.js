fcc81cd2feb07306c3d6bb2ffcfa68e6786f336b
Your Name <you@example.com>
2020-11-03 22:53:23 +0300

    Updates

---
+++ precache-manifest.b2a4b17cbe865464b4b3c8ac1a860843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924382d69e8c9f1a8a599b6e7789382",
+    "url": "/gracie/index.html"
+  },
+  {
+    "revision": "187d8cb31af86a43eb22",
+    "url": "/gracie/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "f8a3952555265afad7ff",
+    "url": "/gracie/static/css/main.b2d9bed7.chunk.css"
+  },
+  {
+    "revision": "187d8cb31af86a43eb22",
+    "url": "/gracie/static/js/2.4c0a0ff1.chunk.js"
+  },
+  {
+    "revision": "f8a3952555265afad7ff",
+    "url": "/gracie/static/js/main.3e77bc19.chunk.js"
+  },
+  {
+    "revision": "6cf8c239dda7e96fd4a5",
+    "url": "/gracie/static/js/runtime~main.9d0acb66.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/gracie/static/media/audio.d41d8cd9.less"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gracie/precache-manifest.cb7cf41c391894c8971ae8c7a1ab6108.js"
+  "/gracie/precache-manifest.b2a4b17cbe865464b4b3c8ac1a860843.js"
 );
 
 self.addEventListener('message', (event) => {
