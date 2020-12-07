0cd76a8af724d349f79948a41f861984a8aac2aa
Your Name <you@example.com>
2020-12-07 19:11:13 +0300

    lp

--- src/pages/about1/about.js
+++ src/pages/about1/about.js
@@ -1,12 +1,32 @@
 import React from 'react'
-
+import './about.css'
 function About() {
     return (
-        <div>
+        <div className="aboutB">
            <h2>About Blogger</h2> 
-           <p>Grace Kanyasya is the author and blogger of The Pizzes blog page. Grace is currently pursuing a degree in Communication and Public Relations at Moi University. She is a passionate inspirational writer, former and founder of The Weekly Zag youth blog, and former writer at Tunda Website. She is also an academic writer, who takes to heart matters concerning research, study, and knowledge. Above all else, a lady who lives and stands for God, and believes in the profound power of truth, knowledge, and wisdom.
-
+<div className="tr" >
+    <p>
+A lot of People know me as Kanyasya, Kanyas, Grace, among many other name inventions that relate to my identity.
+ However, my proper complete name is Grace Mwende Kanyasya. A girl whose loves music, especially the type that speaks to
+  my soul. I love seeing people happy and at peace. I love everything about good hair, and I keep a lot of designer 
+  friends because they are very interesting people, whose level of creativity intrigues me. </p>
+  <p>I enjoy fierce business 
+  and financial conversations. I love adventure, events and social connections.  I love listening to different 
+  perspectives and mentalities on different things. I like to eat, pizza being my favorite (utterly not the reason
+   I named this blog Pizzes, something you’d actually know if you checked out; About Pizzes). Chicken for me comes 
+   right after pizza, so if you were to add one to two, you’d figure out that Chicken barbeque is my pizza jam.</p>
+   <p>
+    I appreciate anyone and anything that challenges my thinking, beliefs, and ideologies. I love solving problems,
+     issues. I enjoy real in-depth conversations, and cheers to all paragraph texters, same Whatsapp group guys!
+      I honor relationships, the greatest one being, the one I have with God. He is my everything. Everything.
+Above all, I value truth.   I am a passionate inspirational writer, former and founder of The Weekly Zag youth blog, 
+and former writer at Tunda Website. I’m also an academic writer, who takes to heart matters concerning research, 
+study, and knowledge.</p>
+<p>
+A Welcome to Pizzes! A place where we re-define, renew, and transform people, mindsets, and lives into greatness.
 </p>
+</div>
+
         </div>
     )
 }
