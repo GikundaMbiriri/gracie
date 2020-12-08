15b17178762b8236a02afdd6a537087398c2b31f
Your Name <you@example.com>
2020-12-08 12:43:27 +0300

    lp

--- src/pages/solo.js
+++ src/pages/solo.js
@@ -2,6 +2,10 @@ import React, { useEffect,useState } from "react";
 import { getScream,Comment } from "../redux/actions/dataAction";
 import { connect } from "react-redux";
 import './solo.css'
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import dayjs from 'dayjs'
+
 const Solo =  (props) => {
   const [name,setName]=useState('');
   const [email,setEmail]=useState('');
@@ -18,43 +22,44 @@ const  onEmailChange = (event) => {
   useEffect( () =>  {
    props.getScream(props.match.params.id);
 
-  }, []);
-const  handleSubmit=()=>{
+  }, [props.scream.comments]);
+const  handleSubmit=(event)=>{
+	event.preventDefault()
+
     const variables={
       name,email,message
     }
     props.Comment(variables,props.match.params.id)
     
-    
+	setMessage('')
+	setName('')
+	setEmail('')
   }
 let k=<p>loading</p>
+dayjs.extend(relativeTime);
+
   const {body,createdAt,loading,comments}=props.scream
-  console.log(body)
   if(comments){
-    console.log(comments)
      k= comments.map((comment)=>
    ( <>
-	<h1 class="comments-title">Comments (3)</h1>
-	<div class="be-comment">
-		<div class="be-img-comment">	
+	<div className="be-comment">
+		<div className="be-img-comment">	
 			<a href="blog-detail-2.html">
-				<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="be-ava-comment"/>
+				<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" className="be-ava-comment"/>
 			</a>
 		</div>
-		<div class="be-comment-content">
+		<div className="be-comment-content">
 			
-				<span class="be-comment-name">
-					<a href="blog-detail-2.html">Ravi Sah</a>
+				<span className="be-comment-name">
+   <a href="blog-detail-2.html">{comment.name}</a>
 					</span>
-				<span class="be-comment-time">
-					<i class="fa fa-clock-o"></i>
-					May 27, 2015 at 3:14am
+				<span className="be-comment-time">
+					<i className="fa fa-clock-o"></i>
+					{dayjs(comment.createdAt).fromNow()}
 				</span>
 
-			<p class="be-comment-text">
-				Pellentesque gravida tristique ultrices. 
-				Sed blandit varius mauris, vel volutpat urna hendrerit id. 
-				Curabitur rutrum dolor gravida turpis tristique efficitur.
+			<p className="be-comment-text">
+				{comment.body}
 			</p>
 		</div>
 	</div></>))
@@ -62,35 +67,37 @@ let k=<p>loading</p>
   }
   let p=null;
   if(body){
-    p=<>  <form class="form-block">
-		<div class="row">
-			<div class="col-xs-12 col-sm-6">
-				<div class="form-group fl_icon">
-					<div class="icon"><i class="fa fa-user"></i></div>
-					<input class="form-input" type="text" placeholder="Your name"/>
+    p=<>  <form className="form-block yt">
+		<h4>leave a comment</h4>
+		<div >
+			<div>
+				<div className="form-group ">
+					<input class="form-control" onChange={onNameChange} value={name} type="text" placeholder="Your name"/>
 				</div>
 			</div>
-			<div class="col-xs-12 col-sm-6 fl_icon">
-				<div class="form-group fl_icon">
-					<div class="icon"><i class="fa fa-envelope-o"></i></div>
-					<input class="form-input" type="text" placeholder="Your email"/>
+			<div>
+			<div class="form-group">
+    <input type="email" class="form-control" onChange={onEmailChange} value={email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+			</div>
+			<div >									
+				<div className="form-group">
+					<textarea className="form-control" onChange={onMessageChange} value={message} required="" placeholder="Your text"></textarea>
 				</div>
 			</div>
-			<div class="col-xs-12">									
-				<div class="form-group">
-					<textarea class="form-input" required="" placeholder="Your text"></textarea>
-				</div>
-			</div>
-			<a class="btn btn-primary pull-right">submit</a>
+			
 		</div>
+		<button className="btn btn-primary" onClick={handleSubmit}>submit</button>
+
 	</form></>
   }
  
-console.log(comments)
-  return( <><div dangerouslySetInnerHTML={{ __html: body }} />
-  
-<div>  <div class="be-comment-block">
-{k}  <div>{p}</div>
+  return( <><div className="greatness" dangerouslySetInnerHTML={{ __html: body }} />
+
+
+<div>  <div className="be-comment-block">
+{p}  <div>{k}</div>
 </div></div>
 
   </>);
